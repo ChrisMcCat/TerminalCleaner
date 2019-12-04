39f00c8ef8719cf256362f41fb167e3b7e7403ea
Chris <ichris.kk@gmail.com>
2019-12-04 11:11:36 +0100

    Exercises

--- src/com/infoshare/jjdd8/Arrays/Array.java
+++ src/com/infoshare/jjdd8/Arrays/Array.java
@@ -14,5 +14,18 @@ public class Array {
         System.out.println(Arrays.toString(arrayTwo));
         Arrays.sort(arrayTwo);
         System.out.println(Arrays.toString(arrayTwo));
+
+
+        //dodanie nowego elementu na poczatek listy
+        int[] arrayThree = new int[] {3, 5, 3, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(arrayThree));
+        int[] arrayThreeCopy = Arrays.copyOf(arrayThree, 8);
+        System.out.println(Arrays.toString(arrayThreeCopy));
+        for (int i = 0; i<arrayThreeCopy.length-1; i++) {
+            arrayThreeCopy[arrayThreeCopy.length-1-i]=arrayThreeCopy[arrayThreeCopy.length-2-i];
+        }
+        System.out.println(Arrays.toString(arrayThreeCopy));
+        arrayThreeCopy[0] = 5;
+        System.out.println(Arrays.toString(arrayThreeCopy));
     }
 }
