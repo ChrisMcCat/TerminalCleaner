64066da3bdfdfac4c1afc40ee8d45fb86c567d2e
Chris <ichris.kk@gmail.com>
2019-12-05 16:49:35 +0100

    Exercises

---
+++ src/main/java/Sets/SetExamples.java
@@ -0,0 +1,28 @@
+package Sets;
+
+import Lists.Person;
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class SetExamples {
+    public static void main(String[] args) {
+        linkedHashSet();
+//        treeSet();
+    }
+
+//    private static void treeSet() {
+//
+//        SetExamples<Person> treePerson = new TreeSet<>();
+//
+//        Person p1 = new Person("Krzysztof", "Kocurek");
+//        Person p2 = new Person("Maciej", "Maciejowski");
+//        Person p3 = new Person("Abraham", "Abrahimowicz");
+//
+//        treePerson.add(p1);
+//
+//    }
+
+    private static void linkedHashSet() {
+    }
+}
