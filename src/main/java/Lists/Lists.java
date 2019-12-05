ce05a23cc30f40a18a538d9e1df72781d464a422
Chris <ichris.kk@gmail.com>
2019-12-05 09:52:38 +0100

    Exercises

--- src/main/java/Lists/Lists.java
+++ src/main/java/Lists/Lists.java
@@ -13,17 +13,19 @@ public class Lists {
         java.util.List<Person> persons_linkedList = new LinkedList<>();
 
 
-        Person p1 = new Person("Przemek", "Grzesiowski");
-        Person p2 = new Person("Alicja", "Nowak");
-        Person p3 = new Person("Tomasz", "Grzesiowski");
+        Person p1 = new Person("Krzysiek", "Kocurek");
+        Person p2 = new Person("Jan", "Nowak");
+        Person p3 = new Person("Piotr", "Musk");
+        Person p4 = new Person("Maciej", "Maciejowski");
 
         persons_arrayList.add(p1);
         persons_arrayList.add(p2);
         persons_arrayList.add(p3);
+        persons_arrayList.add(p4);
 
-        System.out.println(persons_arrayList.indexOf(p1));
+//        System.out.println(persons_arrayList.indexOf(p1));
 
-        persons_arrayList.remove(2);
+//        persons_arrayList.remove(2);
         for (Person person : persons_arrayList) {
             System.out.println(person.getFirstname() + " " + person.getSurname());
         }
