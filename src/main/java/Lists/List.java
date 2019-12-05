06fa356ace03766d860f6fb6f4fa29f2705c023a
Chris <ichris.kk@gmail.com>
2019-12-05 09:07:35 +0100

    Exercises with added maven structure to avoid problems on different workstations

--- src/main/java/Lists/List.java
+++ src/main/java/Lists/List.java
@@ -3,36 +3,36 @@ package Lists;
 import java.util.ArrayList;
 
 public class List {
-
-    public static void main(String[] args) {
-
-
-        List<Person> persons_arrayList = new ArrayList<>();
-        List<Person> persons_linkedList = new LinkedList<>();
-
-
-        Person p1 = new Person("Przemek", "Grzesiowski");
-        Person p2 = new Person("Alicja", "Nowak");
-        Person p3 = new Person("Tomasz", "Grzesiowski");
-
-        persons_arrayList.add(p1);
-        persons_arrayList.add(p2);
-        persons_arrayList.add(p3);
-
-        System.out.println(persons_arrayList.indexOf(p1));
-
-        persons_arrayList.remove(2);
-        for (Person person :persons_arrayList) {
-            System.out.println(person.getFirstname() + " " + person.getSurname());
-        }
-
-
-/*
-        for (Person person : persons_arrayList) {
-            System.out.println(person.getFirstname() + " " + person.getSurname());
-        }*/
-
-
-        //System.out.println(persons_arrayList.size());
-    }
-}
+//
+//    public static void main(String[] args) {
+//
+//
+//        List<Person> persons_arrayList = new ArrayList<>();
+//        List<Person> persons_linkedList = new LinkedList<>();
+//
+//
+//        Person p1 = new Person("Przemek", "Grzesiowski");
+//        Person p2 = new Person("Alicja", "Nowak");
+//        Person p3 = new Person("Tomasz", "Grzesiowski");
+//
+//        persons_arrayList.add(p1);
+//        persons_arrayList.add(p2);
+//        persons_arrayList.add(p3);
+//
+//        System.out.println(persons_arrayList.indexOf(p1));
+//
+//        persons_arrayList.remove(2);
+//        for (Person person :persons_arrayList) {
+//            System.out.println(person.getFirstname() + " " + person.getSurname());
+//        }
+//
+//
+///*
+//        for (Person person : persons_arrayList) {
+//            System.out.println(person.getFirstname() + " " + person.getSurname());
+//        }*/
+//
+//
+//        //System.out.println(persons_arrayList.size());
+//    }
+//}
