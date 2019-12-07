264be66c55cd62b955ce64fe189092183c52f835
ChrisMcCat <57917743+ChrisMcCat@users.noreply.github.com>
2019-12-07 19:47:12 +0100

    Method which checks for os and clean terminal

--- src/main/java/com/jjdd8_ism/HellPrinter.java
+++ src/main/java/com/jjdd8_ism/HellPrinter.java
@@ -2,7 +2,7 @@ package com.jjdd8_ism;
 
 public class HellPrinter {
 
-	private static final String hellprin = "Hell is warm";
+	private static final String hellprin = "Hell is burning";
 
 	public static void HellPrinter(){
 		System.out.println(hellprin);
