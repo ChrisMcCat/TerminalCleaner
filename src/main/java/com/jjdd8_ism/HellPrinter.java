db949413808e086bc0dd8d83cd5ac89401664b75
ChrisMcCat <57917743+ChrisMcCat@users.noreply.github.com>
2019-12-07 19:46:12 +0100

    Update HellPrinter.java

--- src/main/java/com/jjdd8_ism/HellPrinter.java
+++ src/main/java/com/jjdd8_ism/HellPrinter.java
@@ -2,7 +2,7 @@ package com.jjdd8_ism;
 
 public class HellPrinter {
 
-	private static final String hellprin = "sadghkkdshglksdhgksahgdl";
+	private static final String hellprin = "Hell is warm";
 
 	public static void HellPrinter(){
 		System.out.println(hellprin);
